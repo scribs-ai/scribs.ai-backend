06299094d2d1acfb6ae1328acdbec44f912124fb
Kapil Patel <kapil@scribs.ai>
2023-12-19 13:38:41 +0530

    added user controller and jwt concern

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  include JsonWebToken
+	before_action :authenticate_request
+
+	private
+  def authenticate_request
+   header = request.headers['Authorization']
+   header = header.split(" ").last if header
+   decoded  = JWT.decode(header)
+   @current_user  =User.find(decoded[:user_id])  
+  end
 end
