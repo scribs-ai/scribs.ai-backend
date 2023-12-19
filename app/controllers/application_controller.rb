cd535948af662b53eebde2e4fe3a70746a015b51
Kapil Patel <kapil@scribs.ai>
2023-12-19 14:01:14 +0530

    hot fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,13 @@
 class ApplicationController < ActionController::Base
   include JsonWebToken
 	before_action :authenticate_request
+	protect_from_forgery with: :null_session
 
 	private
   def authenticate_request
    header = request.headers['Authorization']
    header = header.split(" ").last if header
-   decoded  = JWT.decode(header)
+   decoded  = jwt_decode(header)
    @current_user  =User.find(decoded[:user_id])  
   end
 end
