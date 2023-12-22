8306834fbec6cdea9cda1bfb6e861d2edb32b26f
Kapil Patel <kapil@scribs.ai>
2023-12-22 18:24:29 +0530

    hot fixes

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,6 @@ class User < ApplicationRecord
 
   def self.create_user_for_google(data)
     where(email: data["email"]).first_or_initialize.tap do |user|
-      user.provider="google_oauth2"
       user.email=data["email"]
       user.password=Devise.friendly_token[0,20]
       user.password_confirmation=user.password
