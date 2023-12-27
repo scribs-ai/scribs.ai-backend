c9185543bfc73304c5d3e511ee8e8bf489680a03
Kapil Patel <kapil@scribs.ai>
2023-12-27 10:33:13 +0530

    method name changes for export user data

--- app/controllers/settings/accounts_controller.rb
+++ app/controllers/settings/accounts_controller.rb
@@ -1,13 +1,18 @@
 class Settings::AccountsController < ApplicationController
 	skip_before_action :verify_authenticity_token
 
-	def export_user_data_to_csv
-		users_data = User.all
-		send_data User.to_csv,
-                    filename: "user_data_#{Time.zone.now.strftime('%Y%m%d%H%M%S')}.csv",
-                    type: 'text/csv; charset=utf-8',
-                    disposition: 'attachment'
-	end
+  def export_user_data
+    users_data = User.all
+    respond_to do |format|
+      format.csv do
+        send_data User.to_csv,
+                  filename: "user_data_#{Time.zone.now.strftime('%Y%m%d%H%M%S')}.csv",
+                  type: 'text/csv; charset=utf-8',
+                  disposition: 'attachment'
+      end
+      format.json { render json: users_data }
+    end
+  end
 
 	def delete_account
 		user = User.find_by_email(params[:email])
