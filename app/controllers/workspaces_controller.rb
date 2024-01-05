42adc88c95d520114331232c7c53d8d504e4750a
Kapil Patel <kapil@scribs.ai>
2024-01-05 14:36:51 +0530

    to create directory at S3 bucket on workspace creation

--- app/controllers/workspaces_controller.rb
+++ app/controllers/workspaces_controller.rb
@@ -1,4 +1,6 @@
 class WorkspacesController < ApplicationController
+  require 'aws-sdk-s3'
+
   before_action :set_workspace, only: [:show, :update, :destroy]
 
   def index
@@ -13,6 +15,9 @@ class WorkspacesController < ApplicationController
   def create
     @workspace = Workspace.new(workspace_params)
     if @workspace.save
+      s3_client = get_s3_client
+      directory = create_directory(s3_client)
+
       render json: @workspace, status: :created
     else
       render json: @workspace.errors, status: :unprocessable_entity
@@ -46,8 +51,35 @@ class WorkspacesController < ApplicationController
 		end
 	end
 	
-
   def workspace_params
     params.require(:workspace).permit(:name, :archived)
+  end
+
+  def get_s3_client
+    s3 = Aws::S3::Client.new(
+    access_key_id: Rails.application.credentials.dig(:aws, :access_key_id),
+    secret_access_key: Rails.application.credentials.dig(:aws, :secret_access_key),
+    region: Rails.application.credentials.dig(:aws, :region)
+  )
+  end
+
+  def create_directory(s3_client)
+    directory_name = @workspace.name
+    bucket_name = Rails.application.credentials.dig(:aws, :bucket)
+    directory_key = "#{directory_name}/"
+
+    head_object_response = s3_client.head_object(
+      bucket: bucket_name,
+      key: directory_key
+    )
+
+    return Aws::S3::Object.new(bucket_name, directory_key) if head_object_response
+
+    s3_client.put_object(
+      bucket: bucket_name,
+      key: directory_key
+    )
+
+    Aws::S3::Object.new(bucket_name, directory_key)
   end
 end
