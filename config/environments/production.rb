e38391e4309d48b948c8929183ee70e596116821
Kapil Patel <kapil@scribs.ai>
2024-01-09 16:47:05 +0530

    modify database.yml and smtp configuration for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -49,7 +49,7 @@ Rails.application.configure do
   # config.assume_ssl = true
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  config.force_ssl = true
+  config.force_ssl = false
 
   # Log to STDOUT by default
   config.logger = ActiveSupport::Logger.new(STDOUT)
@@ -94,4 +94,14 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'google.com', # Change this to your domain
+    user_name:            'kapil@scribs.ai',  # Change this to your Gmail email
+    password:             'owtt vxlf ccba bafh',    # Change this to your Gmail password or app-specific password
+    authentication:       'plain',
+    enable_starttls_auto: true
+  }
 end
